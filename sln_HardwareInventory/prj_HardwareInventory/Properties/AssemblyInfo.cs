1d65b54bd4010f023a253b51186806d7e6474cc7
UserProgrammer <michaelcosta_11_17@hotmail.com>
2016-05-22 11:44:36 -0400

    Updated Assemblyinfo.cs

--- sln_HardwareInventory/prj_HardwareInventory/Properties/AssemblyInfo.cs
+++ sln_HardwareInventory/prj_HardwareInventory/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PC_Inventory_Project")]
+[assembly: AssemblyTitle("Hardware Inventory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("PC_Inventory_Project")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyProduct("Hardware Inventory")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
